5cd9f762d2e7559300dec9cf602c39843c5a4f99
Jamie Zhu <Zqijing0815@gmail.com>
2020-05-06 19:57:07 +0000

    Updated functions in markov file

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 """Generate Markov text from text files."""
 
+import sys
 from random import choice
 
 
@@ -10,9 +11,15 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    file = open(file_path)
+    text = file.read()
+    file.close()
 
-    return "Contents of your file as one long string"
+    return text
+
+    # With the with keyword, the file will be automatically closed.
+    # with open(file_path) as file:
+    #     text = file.read()
 
 
 def make_chains(text_string):
@@ -40,9 +47,23 @@ def make_chains(text_string):
         [None]
     """
 
-    chains = {}
+    chains = {} #dictionary
 
-    # your code goes here
+    words = text_string.split()
+
+    words.append(None) #append None to the end of our word list to set a stop point
+
+    for i in range(len(words) - 2): #using len - 2 to aoint out of boundary
+        key = (words[i], words[i + 1])
+        value = words[i + 2]
+
+        if key not in chains:
+            chains[key] = []
+
+        chains[key].append(value)
+
+    # Or replace the last three lines with:
+    # chains.setdefault(key, []).append(value)
 
     return chains
 
@@ -50,14 +71,19 @@ def make_chains(text_string):
 def make_text(chains):
     """Return text from chains."""
 
-    words = []
+    key = choice(list(chains.keys()))
+    words = [key[0], key[1]]
+    word = choice(chains[key])
 
-    # your code goes here
+    while word is not None:
+        key = (key[1], word)
+        words.append(word)
+        word = choice(chains[key])
 
     return " ".join(words)
 
 
-input_path = "green-eggs.txt"
+input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
